97c5bd60897559d42ecf0c281c7b5904aec7fbc3
Rahul Das <dasrahulrock2001@gmail.com>
2022-10-01 13:32:04 +0530

    updated counter to change to plural for multiple clicks, increased the click area, changes background colour

--- Button Counter/index.js
+++ Button Counter/index.js
@@ -3,5 +3,12 @@ var button = document.getElementById("counter")
 function clickgenerator() {
     click += 1;
     console.log(click);
-    counter.innerHTML = "You clicked the button " + click + " time";
+    if (click==1) {
+        counter.innerHTML = "You clicked the button " + click + " time";
+    } 
+    
+    else {
+        counter.innerHTML = "You clicked the button " + click + " times";
+    }
+    
 }
