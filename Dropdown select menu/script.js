c9ed645c9d42c3eff6ada70238b06051937b37ec
Siddhant Tripathy <tripathysiddhant208@gmail.com>
2022-10-17 09:34:28 +0530

    Dropdown menu

---
+++ Dropdown select menu/script.js
@@ -0,0 +1,15 @@
+const optionMenu = document.querySelector(".select-menu"),
+       selectBtn = optionMenu.querySelector(".select-btn"),
+       options = optionMenu.querySelectorAll(".option"),
+       sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+    });
+});
